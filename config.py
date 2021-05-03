255a941528218bd22232edefdbdfe6045ce4665f
Fame Chailert <fame_chailert@hotmail.com>
2021-05-03 16:03:33 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'ENTER_STORAGE_ACCOUNT_NAME'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'ENTER_BLOB_STORAGE_KEY'
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'ENTER_IMAGES_CONTAINER_NAME'
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'storageproject1fame'
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'RcXCb0FWWKVDr6ZtLQxwmQoQhYPVxQEyEoo9otYXQI0JypNZRnCiYzamOgU3z35UVjAS83Ci2KeK5ZpxR3f0wg=='
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or 'ENTER_SQL_SERVER_NAME.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'ENTER_SQL_DB_NAME'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'ENTER_SQL_SERVER_USERNAME'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'ENTER_SQL_SERVER_PASSWORD'
+    SQL_SERVER = os.environ.get('SQL_SERVER') or 'database-project-1-fame.database.windows.net'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'Database_Project_I_fame'
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'project1admin'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'QWer@72048'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
